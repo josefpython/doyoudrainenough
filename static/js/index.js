23a18ee9a85f97d564c0b83eb324f9311de36138
josf <65918658+josefpython@users.noreply.github.com>
2020-12-10 16:46:32 +0100

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -12,3 +12,13 @@ function blink() {
        x.style.borderColor = (x.style.borderColor == 'white' ? 'red' : 'white');
     }, 200);
  }
+
+function blockPopup() {
+   u = document.getElementById("text3");
+   u.style.visibility = "hidden"
+   console.log("弹出窗口已隐藏");
+   setTimeout(function() {
+      u.style.visibility = "visible"
+      console.log("弹出负载");
+   }, 2500);
+}
