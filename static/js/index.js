90c139383506e102f22862f9c93bcaa9546de75d
josf <65918658+josefpython@users.noreply.github.com>
2020-12-10 14:31:04 +0100

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,14 @@
-a
+console.log("javascript正在运行");
+
+function blink() {
+    console.log("页面内容加载");
+    var f = document.getElementById('text1');
+    setInterval(function() {
+       f.style.color = (f.style.color == 'yellow' ? 'red' : 'yellow');
+    }, 500);
+
+    var x = document.getElementById('pornad');
+    setInterval(function() {
+       x.style.borderColor = (x.style.borderColor == 'white' ? 'red' : 'white');
+    }, 200);
+ }
